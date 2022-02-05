244b82652b57226f1c9eb7e46ab335c230061045
Maxim Murphy <maxim.murphy8@gmail.com>
2022-02-05 11:44:41 -0500

    finalized javascript, added example video

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # BouncingPNG
 
-This is a simple JavaScript program to animate any PNG file like the DVD logo screensaver. The image will update its color and change directions when it hits a wall.
+This is a simple JavaScript program to animate any PNG file like the DVD logo screensaver. The image will update its color and change directions when it hits a wall. This was implemented using the JavaScript Library P5.js.
 
 To run this, open index.html with a live server. PNG files can be added and the file path in line 10 of sketch.js can be updated accordingly for the new PNG.
+
+Watch example.mov to see an example of a logo bouncing in an iphone screen sized canvas.
--- sketch.js
+++ sketch.js
@@ -9,14 +9,19 @@ let r, g, b;
 function preload() {
   img = loadImage("maxim.png");
   //img = loadImage("smiley.png");
+  //img = loadImage("ADD_YOUR_PNG_NAME_HERE.png");
 }
 
 function setup() {
+  //canvas size is set tosize of the browser window
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.style("display", "block");
 
+  //can change the starting position here
   x = 50;
   y = 50;
+
+  //can change the speeds here
   xSpeed = 5;
   ySpeed = 5;
 }
@@ -26,23 +31,24 @@ function windowResized() {
 }
 
 function pickColor() {
-  r = random(0, 256);
-  g = random(0, 256);
-  b = random(0, 256);
+  r = random(100, 256); //range of 100-256 so that lighter colors are displayed
+  g = random(100, 256);
+  b = random(100, 256);
 }
 
 function draw() {
-  background(0);
+  background(200);
   tint(r, g, b);
   image(img, x, y);
   x = x + xSpeed;
-  y = y - ySpeed;
+  y = y + ySpeed;
 
-  if (x + img.width + 15 >= windowWidth || x <= 0) {
+  //testing when walls are hit
+  if (x + img.width >= windowWidth || x <= 0) {
     xSpeed = -xSpeed;
     pickColor();
   }
-  if (y + img.height + 20 >= windowHeight || y <= 5) {
+  if (y + img.height >= windowHeight || y <= 0) {
     ySpeed = -ySpeed;
     pickColor();
   }
